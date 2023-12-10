1bd63b982aebc95e088a681a0cbdd36a08d89098
Santirat138 <pleumsantirat@gmail.com>
2023-12-11 00:42:25 +0700

    Create DataStruct_cirSingle.cpp

---
+++ C++/DataStruct_cirSingle.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+class node{
+    public:
+        int num;
+        node *next;
+
+        node(int newNum){
+            num=newNum;
+            next=NULL;
+        }
+};
+class cirLinkList{
+    public:
+        node *nullNode=new node(-1);
+        node *head;
+        node *tail;
+
+        
+};
+int main(){
+
+}
