85fee91e1ecc4b91a564b2c46dbacda10c8de51e
Santirat138 <pleumsantirat@gmail.com>
2023-12-14 18:01:24 +0700

    Create DataStruct_CloseHash.java

---
+++ Java/DataStruct_CloseHash.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class DataStruct_CloseHash {
+    public static void main(String[] args){
+        Scanner input=new Scanner(System.in);
+        int size=input.nextInt();
+        int[] array=new int[size];
+        int num, idx;
+        int i=0;
+        do{
+            System.out.printf("Enter number. ");
+            num=input.nextInt();
+            idx=num%size;
+            array[idx]=num;
+            i++;
+        }
+        while(i<size);
+        for(i=0;i<size;i++){
+            System.out.printf("idx %d, number %d\n", i, array[i]);
+        }
+    }
+}
