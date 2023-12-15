7d2b93de1164df647b09025defec0a08dc17379a
Santirat138 <pleumsantirat@gmail.com>
2023-12-15 08:34:47 +0700

    Update DataStruct_CloseHash.java

--- Java/DataStruct_CloseHash.java
+++ Java/DataStruct_CloseHash.java
@@ -1,6 +1,6 @@
+// Close hash use array.
 import java.util.Scanner;
-
-public class DataStruct_CloseHash {
+public class DataStruct_CloseHash{
     public static void main(String[] args){
         Scanner input=new Scanner(System.in);
         System.out.printf("Enter size. ");
@@ -8,21 +8,31 @@ public class DataStruct_CloseHash {
         int[] array=new int[size];
         int num, idx;
         int i=0;
+        for(int a=0;a<size;a++){
+            array[a]=-1;
+        }
         do{
             System.out.printf("Enter number. ");
             num=input.nextInt();
             idx=num%size;
-            if(array[idx]==0){
-                array[idx]=num;
+            if(num!=-1){
+                if(array[idx]==-1){
+                    array[idx]=num;
+                }
+                else if(array[idx]!=-1){
+                    System.out.printf("Index %d is not empty.\n", idx);
+                    idx=idx+1;
+                    array[idx]=num;
+                }
+                i++;
             }
-            else{
-                System.out.println("Error");
+            else if(num==-1){
+                break;
             }
-            i++;
         }
         while(i<size);
-        for(i=0;i<size;i++){
-            System.out.printf("idx %d, number %d\n", i, array[i]);
+        for(int k=0;k<size;k++){
+            System.out.printf("%d ", array[k]);
         }
     }
 }
