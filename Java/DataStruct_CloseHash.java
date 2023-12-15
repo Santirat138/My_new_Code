256ddb4d34feddc7e76a0cd6aaff6eaa8cebcd07
Santirat138 <pleumsantirat@gmail.com>
2023-12-15 13:32:39 +0700

    Update DataStruct_CloseHash.java

--- Java/DataStruct_CloseHash.java
+++ Java/DataStruct_CloseHash.java
@@ -1,39 +1,68 @@
 // Close hash use array.
-import java.util.Scanner;
 public class DataStruct_CloseHash{
     public static void main(String[] args){
-        Scanner input=new Scanner(System.in);
-        System.out.printf("Enter size. ");
-        int size=input.nextInt();
-        int[] array=new int[size];
-        int num, idx;
-        int i=0;
-        for(int a=0;a<size;a++){
-            array[a]=-1;
+        int size=5;
+        table hash1=new table(size);
+        hash1.insert(7);
+        hash1.insert(22);
+        hash1.insert(5);
+        hash1.insert(25);
+        hash1.show();
+        hash1.deleteNum(25);
+        hash1.show();
+    }
+}
+class table{
+    int size;
+    int[] array;
+    int idx;
+    int num;
+    
+    table(int sizeIn){
+        num=-1;
+        size=sizeIn;
+        array=new int[size];
+        for(int i=0;i<size;i++){
+            array[i]=num;
         }
-        do{
-            System.out.printf("Enter number. ");
-            num=input.nextInt();
-            idx=num%size;
-            System.out.printf("%d add in idx %d.\n", num, idx);
-            if(num!=-1){
+    }
+    int findIdx(int numIn){
+        return numIn%size;
+    }
+    void insert(int newNum){
+        num=newNum;
+        idx=findIdx(num);
+        if((idx<size)&&(idx>=0)){
+            while(idx<size){
                 if(array[idx]==-1){
-                    array[idx]=num;
+                    array[idx]=newNum;
+                    break;
                 }
-                else if(array[idx]!=-1){
-                    System.out.printf("Index %d is not empty.\n", idx);
-                    idx=idx+1;
-                    array[idx]=num;
+                else{
+                    idx++;
                 }
-                i++;
-            }
-            else if(num==-1){
-                break;
             }
         }
-        while(i<size);
-        for(int k=0;k<size;k++){
-            System.out.printf("%d ", array[k]);
+        else{
+            System.out.println("Error");
+        }
+    }
+    void show(){
+        System.out.printf("\n");
+        for(int i=0;i<size;i++){
+            System.out.printf("%d ", array[i]);
+        }
+        System.out.printf("\n");
+    }
+    void deleteNum(int target){
+        int currIdx=findIdx(target);
+        while(currIdx<size){
+            if(array[currIdx]==target){
+                array[currIdx]=-1;
+            }
+            else{
+                currIdx++;
+            }
         }
     }
 }
