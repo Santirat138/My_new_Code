a8607ab23313d2df17b47fcb698f6a5fb07daa5a
Santirat138 <pleumsantirat@gmail.com>
2023-12-17 01:31:29 +0700

    Update My_LinkList.java

--- Java/My_LinkList.java
+++ Java/My_LinkList.java
@@ -1,3 +1,32 @@
 public class My_LinkList {
-    
+    public static void main(String[] args){
+        
+    }
+}
+class node{
+    int num;
+    node next;
+
+    node(int numIn){
+        num=numIn;
+        next=null;
+    }
+}
+class linkList{
+    node nullNode=new node(-1);
+    node head;
+
+    linkList(){
+        head=null;
+    }
+    void addFirst(int newNum){
+        node newNode=new node(newNum);
+        if(head!=null){
+            newNode.next=head;
+        }
+        head=newNode;
+    }
+    void show(){
+        
+    }
 }
