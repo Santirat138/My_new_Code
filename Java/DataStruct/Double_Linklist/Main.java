dfa74d2ee382cdd9b0d56e6e80961e2f7330c762
Santirat138 <pleumsantirat@gmail.com>
2023-12-17 01:57:52 +0700

    Update Main.java

--- Java/DataStruct/Double_Linklist/Main.java
+++ Java/DataStruct/Double_Linklist/Main.java
@@ -2,6 +2,24 @@ package DataStruct.Double_Linklist;
 
 public class Main {
     public static void main(String[] args){
-        
+
     }
+}
+class node{
+    int num;
+    node prev;
+    node next;
+
+    node(int numIn){
+        num=numIn;
+        prev=null;
+        next=null;
+    }
+}
+class linkList{
+    node nullNode=new node(-1);
+    node head;
+    node tail;
+
+    
 }
