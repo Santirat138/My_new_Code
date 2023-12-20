387aefdede4b068c1816be56327c1c191cc1a45f
Santirat138 <pleumsantirat@gmail.com>
2023-12-20 20:44:19 +0700

    Update Main.java

--- Java/DataStruct/HashTable/Open_Hash/Main.java
+++ Java/DataStruct/HashTable/Open_Hash/Main.java
@@ -1,5 +1,42 @@
 package DataStruct.HashTable.Open_Hash;
 
 public class Main {
-    
+    public static void main(String[] args){
+
+    }
+}
+class node{
+    int num;
+    node next;
+
+    node(int numIn){
+        num=numIn;
+        next=null;
+    }
+}
+class linkList{
+    node head;
+
+    linkList(){
+        head=null;
+    }
+    void insertNum(int newNum){
+        node newNode=new node(newNum);
+        if(head!=null){
+            newNode.next=head;
+        }
+        head=newNode;
+    }
+}
+class hashTable{
+    int arraySize, key;
+    linkList[] array;
+
+    hashTable(int sizeIn){
+        arraySize=sizeIn;
+        array=new linkList[arraySize];
+        for(int i=0;i<arraySize;i++){
+            array[i]=new linkList();
+        }
+    }
 }
