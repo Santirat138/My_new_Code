b47436e4d7ad7e58b06463a88bb1a78ffcba4958
Santirat138 <pleumsantirat@gmail.com>
2023-12-21 19:08:20 +0700

    Update Main.java

--- Java/DataStruct/Test_Space/Way_1/Main.java
+++ Java/DataStruct/Test_Space/Way_1/Main.java
@@ -2,7 +2,7 @@ package DataStruct.Test_Space.Way_1;
 
 public class Main {
     public static void main(String[] args){
-
+        
     }
 }
 class node{
@@ -19,6 +19,20 @@ class linkList{
     
     linkList(){
         head=null;
+    }
+    void addFirst(int newNum){
+        node newNode=new node(newNum);
+        if(head!=null){
+            newNode.next=head;
+        }
+        head=newNode;
+    }
+    void show(){
+        node currNode=head;
+        while(currNode!=null){
+            System.out.printf("%d ", currNode.num);
+            currNode=currNode.next;
+        }
     }
 }
 class stack{
@@ -58,5 +72,79 @@ class queue{
             lastNode.next=newNode;
         }
         lastNode=newNode;
+    }
+    node dequeue(){
+        node temp=firstNode;
+        while(temp!=null){
+            if(temp.next==lastNode){
+                break;
+            }
+            else{
+                temp=temp.next;
+            }
+        }
+        lastNode=temp;
+        temp=lastNode.next;
+        lastNode.next=null;
+        return temp;
+    }
+}
+class arrayLLBox{
+    int size;
+    linkList[] arrayLL;
+
+    arrayLLBox(int sizeIn){
+        size=sizeIn;
+        arrayLL=new linkList[size];
+        for(int i=0;i<size;i++){
+            arrayLL[i]=new linkList();
+        }
+    }
+    void insertNum(int idx, int newNum){
+        arrayLL[idx].addFirst(newNum);
+    }
+    void showIdx(int idx){
+        arrayLL[idx].show();
+    }
+    void showAll(){
+        for(int i=0;i<size;i++){
+            arrayLL[i].show();
+        }
+    }
+}
+class arrayStackBox{
+    int size;
+    stack[] arrayStack;
+
+    arrayStackBox(int sizeIn){
+        size=sizeIn;
+        arrayStack=new stack[size];
+        for(int i=0;i<size;i++){
+            arrayStack[i]=new stack();
+        }
+    }
+    void pushInIdx(int idx, int newNum){
+        arrayStack[idx].push(newNum);
+    }
+    node popInIdx(int idx){
+        return arrayStack[idx].pop();
+    }
+}
+class arrayQueueBox{
+    int size;
+    queue[] arrayQueue;
+
+    arrayQueueBox(int sizeIn){
+        size=sizeIn;
+        arrayQueue=new queue[size];
+        for(int i=0;i<size;i++){
+            arrayQueue[i]=new queue();
+        }
+    }
+    void enqueueInIdx(int idx, int newNum){
+        arrayQueue[idx].enqueue(newNum);
+    }
+    node dequeueInIdx(int idx){
+        return arrayQueue[idx].dequeue();
     }
 }
