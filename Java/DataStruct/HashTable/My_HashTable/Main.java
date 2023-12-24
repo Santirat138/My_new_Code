dc167baaa48c1392ed9922db9c195c64b4dd9ed2
Santirat138 <pleumsantirat@gmail.com>
2023-12-24 11:59:19 +0700

    Open Hash Table(2.0LSQ)

--- Java/DataStruct/HashTable/My_HashTable/Main.java
+++ Java/DataStruct/HashTable/My_HashTable/Main.java
@@ -1,92 +1,138 @@
+package DataStruct.HashTable.My_HashTable;
+// Open Hash Table(2.0LSQ)
 public class Main{
     public static void main(String[] args){
-        hashTable table1=new hashTable(5);
-        table1.insertNum(5);
-        table1.insertNum(50);
-        table1.insertNum(55);
-        table1.insertNum(1);
-        table1.insertNum(6);
-        table1.insertNum(54);
-        table1.showAll();
+        hashTable table=new hashTable(5);
+        table.push(1);
+        table.push(5);
+        table.push(6);
+        table.push(15);
+        table.push(10);
+        table.showStkAll();
     }
 }
 class node{
     int num;
+    node prev;
     node next;
 
     node(int numIn){
         num=numIn;
+        prev=null;
         next=null;
     }
 }
-class queue{
-    node firstNode;
-    node lastNode;
-    queue(){
-        firstNode=null;
-        lastNode=null;
+class linkList{
+    node head;
+    node tail;
+    node nullNode;
+
+    linkList(){
+        head=null;
+        tail=null;
+        nullNode=new node(-1);
     }
-    void buildFirstNode(int newNum){
-        node newNode=new node(newNum);
-        firstNode=newNode;
-        lastNode=newNode;
+    void connectNode(node node1, node node2){
+        node1.next=node2;
+        node2.prev=node1;
     }
-    void enqueue(int newNum){
+    void addFirst(int newNum){
         node newNode=new node(newNum);
-        newNode.next=lastNode;
-        lastNode=newNode;
+        if((head!=null)&&(tail!=null)){
+            connectNode(newNode, head);
+        }
+        else{
+            tail=newNode;
+        }
+        head=newNode;
+    }
+    int pop(){
+        node temp=head;
+        if(head!=tail){
+            head=head.next;
+            head.prev=null;
+            temp.next=null;
+        }
+        else{
+            head=nullNode;
+        }
+        return temp.num;
+    }
+    void addLast(int newNum){
+        node newNode=new node(newNum);
+        if(head==null){
+            head=newNode;
+        }
+        else{
+            connectNode(tail, newNode);
+        }
+        tail=newNode;
     }
     int dequeue(){
-        node temp=lastNode;
-        while(temp!=null){
-            if(temp.next==firstNode){
-                break;
-            }
-            else if(temp.next==null){
-                return temp.num;
-            }
-            else{
-                temp=temp.next;
-            }
+        node temp=tail;
+        if(head!=tail){
+            temp=tail;
+            tail=tail.prev;
+            tail.next=null;
+            temp.prev=null;
         }
-        firstNode=temp;
-        temp=temp.next;
-        firstNode.next=null;
+        else{
+            temp.num=tail.num;
+            tail=nullNode;
+        }
         return temp.num;
+    }
+    void showHeadTail(){
+        node currNode=head;
+        while(currNode!=null){
+            System.out.printf("%d ", currNode.num);
+            currNode=currNode.next;
+        }
+    }
+    void showTailHead(){
+        node currNode=tail;
+        while(currNode!=null){
+            System.out.printf("%d ", currNode.num);
+            currNode=currNode.prev;
+        }
     }
 }
 class hashTable{
     int size, key;
-    queue[] arrayQ;
+    linkList[] arrayStk;
+    linkList[] arrayQ;
 
     hashTable(int sizeIn){
         size=sizeIn;
-        arrayQ=new queue[size];
+        arrayStk=new linkList[size];
+        arrayQ=new linkList[size];
         for(int i=0;i<size;i++){
-            arrayQ[i]=new queue();
+            arrayStk[i]=new linkList();
+            arrayQ[i]=new linkList();
         }
     }
     int findKey(int numIn){
         return numIn%size;
     }
-    void insertNum(int newNum){
+    void push(int newNum){
         key=findKey(newNum);
-        if(arrayQ[key].firstNode==null){
-            arrayQ[key].buildFirstNode(newNum);
-        }
-        else{
-            arrayQ[key].enqueue(newNum);
+        arrayStk[key].addFirst(newNum);
+    }
+    void enqueue(int newNum){
+        key=findKey(newNum);
+        arrayQ[key].addLast(newNum);
+    }
+    void showStkAll(){
+        for(int i=0;i<size;i++){
+            System.out.printf("Index %d : ", i);
+            arrayStk[i].showHeadTail();
+            System.out.println("");
         }
     }
-    void showAll(){
-        node currNode;
+    void showQAll(){
         for(int i=0;i<size;i++){
-            currNode=arrayQ[i].lastNode;
             System.out.printf("Index %d : ", i);
-            while(currNode!=null){
-                System.out.printf("%d ", currNode.num);
-                currNode=currNode.next;
-            }
+            arrayQ[i].showTailHead();
             System.out.println("");
         }
     }
