d45c9f743a2a6422a194e102b2afce9890f77239
Santirat138 <pleumsantirat@gmail.com>
2024-01-06 02:06:19 +0700

    Update Main.java

--- Java/DataStruct/My_Data_Struct/Main.java
+++ Java/DataStruct/My_Data_Struct/Main.java
@@ -1,14 +1,11 @@
 package DataStruct.My_Data_Struct;
-// Do sort when add new number.
+
 public class Main{
-    public static void main(String[] args){
-        linkList list1=new linkList(5);
-        list1.addNum(10);
-        list1.addNum(25);
-        list1.addNum(20);
-        list1.addNum(30);
-        list1.addNum(2);
-        list1.addNum(7);
+    public static void main(String[] args) {
+        linkList list1=new linkList(10);
+        list1.addSortMinMax(15);
+        list1.addSortMinMax(8);
+        list1.addSortMinMax(20);
         list1.showAll();
     }
 }
@@ -16,8 +13,9 @@ class node{
     int num;
     node prev;
     node next;
-    node(int newNum){
-        num=newNum;
+
+    node(int numIn){
+        num=numIn;
         prev=null;
         next=null;
     }
@@ -28,8 +26,8 @@ class linkList{
     node nullNode;
     int amount;
 
-    linkList(int headNum){
-        head=new node(headNum);
+    linkList(int numIn){
+        head=new node(numIn);
         tail=head;
         nullNode=new node(-1);
         amount=1;
@@ -38,57 +36,154 @@ class linkList{
         n1.next=n2;
         n2.prev=n1;
     }
-    void addNum(int newNum){
-    	node newNode=new node(newNum);
-    	node compNode=head;
-    	if(newNode.num<compNode.num) {
-    		connectNode(newNode ,head);
-    		head=newNode;
-            amount++;
-    	}
-    	else {
-    		while(compNode!=null) {
-    			if(compNode.next==null) {
-    				connectNode(tail, newNode);
-    				tail=newNode;
-                    amount++;
-    				break;
-    			}
-    			else {
-    				if(compNode.next.num>newNode.num) {
-    					connectNode(newNode, compNode.next);
-    					connectNode(compNode, newNode);
-                        amount++;
-    					break;
-    				}
-    				else {
-    					compNode=compNode.next;
-    				}
-    			}
-    		}
-    	}
+    void addFirst(int newNum){
+        node newNode=new node(newNum);
+        connectNode(newNode, head);
+        head=newNode;
+        amount++;
     }
-    void showAtHead(node currNode){
+    void addLast(int newNum){
+        node newNode=new node(newNum);
+        connectNode(tail, newNode);
+        tail=newNode;
+    }
+    void showHead(node currNode){
         if(currNode==null){
             System.out.print(" End.");
         }
         else{
             System.out.printf("%d ", currNode.num);
-            showAtHead(currNode.next);
+            showHead(currNode.next);
         }
     }
-    void showAtTail(node currNode){
+    void showTail(node currNode){
         if(currNode==null){
             System.out.print(" End.");
         }
         else{
             System.out.printf("%d ", currNode.num);
-            showAtTail(currNode.prev);
+            showTail(currNode.prev);
         }
     }
     void showAll(){
-        showAtHead(head);
+        showHead(head);
         System.out.println("");
-        showAtTail(tail);
+        showTail(tail);
+    }
+    node findNumAtHead(node currNode, int numIn){
+        if(currNode==null){
+            return nullNode;
+        }
+        else{
+            if(currNode.num==numIn){
+                return currNode;
+            }
+            else{
+                return findNumAtHead(currNode.next, numIn);
+            }
+        }
+    }
+    node findNumAtTail(node currNode, int numIn){
+        if(currNode==null){
+            return nullNode;
+        }
+        else{
+            if(currNode.num==numIn){
+                return currNode;
+            }
+            else{
+                return findNumAtTail(currNode.prev, numIn);
+            }
+        }
+    }
+    node findPos(node currNode, int currPos, int pos){
+        if(currPos<amount){
+            return tail;
+        }
+        else{
+            if(currPos==pos){
+                return currNode;
+            }
+            else{
+                return findPos(currNode.next, currPos+1, pos);
+            }
+        }
+    }
+    node findNum(int numIn){
+        node midNode=findPos(head, 1, amount/2);
+        if(numIn<midNode.num){
+            return findNumAtHead(head, numIn);
+        }
+        else if(numIn>midNode.num){
+            return findNumAtTail(tail, numIn);
+        }
+        else{
+            return nullNode;
+        }
+    }
+    void addSortMinMax(int newNum){
+        node newNode=new node(newNum);
+        node compNode=head;
+        while(compNode!=null){
+            if(head==tail){
+                if(newNum<compNode.num){
+                    addFirst(newNum);
+                    break;
+                }
+                else if(newNum>compNode.num){
+                    addLast(newNum);
+                }
+                break;
+            }
+            else{
+                if(newNum<compNode.num){
+                    addFirst(newNum);
+                    break;
+                }
+                else if((compNode.num<newNum)&&(compNode.next.num>newNum)){
+                    connectNode(newNode, compNode.next);
+                    connectNode(compNode, newNode);
+                    break;
+                }
+            }
+        }
+    }
+    void addSortMaxMin(int newNum){
+        node newNode=new node(newNum);
+        node compNode=head;
+        while(compNode!=null){
+            if(newNum>compNode.num){
+                addFirst(newNum);
+                break;
+            }
+            else{
+                if((compNode.num<newNum)&&(compNode.next.num>newNum)){
+                    connectNode(compNode, newNode);
+                    connectNode(newNode, compNode);
+                    break;
+                }
+                else{
+                    compNode=compNode.next;
+                }
+            }
+        }
+    }
+    void deleteNum(int numIn){
+        node delNode=findNum(numIn);
+        if(delNode==head){
+            head=head.next;
+            head.prev=null;
+            delNode.next=null;
+        }
+        else if(delNode==tail){
+            tail=tail.prev;
+            tail.next=null;
+            delNode.next=null;
+        }
+        else{
+            connectNode(delNode.prev, delNode.next);
+            delNode.prev=null;
+            delNode.next=null;
+        }
     }
 }
