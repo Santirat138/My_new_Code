f24cf40a84a73a20d07d30421bc5b5af83bdd2bc
Santirat138 <pleumsantirat@gmail.com>
2024-01-06 17:52:15 +0700

    Update Main.java

--- Java/Practice/Ex_1/Main.java
+++ Java/Practice/Ex_1/Main.java
@@ -1,5 +1,16 @@
 package Practice.Ex_1;
 
 public class Main {
-    
+    public static void main(String[] args){
+        int[] array={1, 2, 3, 4, 5};
+        int target=6;
+        for(int i=4;i>=0;i--){
+            for(int j=0;j<i;j++){
+                if(array[i]+array[j]==target){
+                    System.out.printf("Number %d %d\n", array[i], array[j]);
+                    System.out.printf("Index %d %d\n\n", i, j);
+                }
+            }
+        }
+    }
 }
