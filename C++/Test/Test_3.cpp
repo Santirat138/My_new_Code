e9cd810baf0348b38b7a844f652ae8a5243ca9c2
Santirat138 <pleumsantirat@gmail.com>
2024-01-15 16:43:12 +0700

    Update Test_3.cpp

--- C++/Test/Test_3.cpp
+++ C++/Test/Test_3.cpp
@@ -1,1 +1,69 @@
-// Method_1 Use open hash table
+// Method_1 Use open hash table.
+#include<iostream>
+using namespace std;
+class node{
+    public:
+        char ch;
+        node *next;
+
+        node(char charIn);
+};
+class linkList{
+    public:
+        node *head;
+        node *nullNode;
+        
+        linkList();
+        void addFirst(char chIn);
+        int countNode(node *currNode, int amount);
+        void showList(node *currNode);
+};
+class hashTable{
+    public:
+        int key;
+        int size;
+        linkList *arrayChar[];
+
+        hashTable(string txtIn);
+        void showAll();
+};
+node::node(char charIn){
+    ch=charIn;
+    next=NULL;
+}
+linkList::linkList(){
+    head=NULL;
+    nullNode=new node('-');
+}
+void linkList::addFirst(char chIn){
+    node *newNode=new node(chIn);
+    if(head!=NULL){
+        newNode->next=head;
+    }
+    head=newNode;
+}
+int linkList::countNode(node *currNode, int amount){
+    if(currNode==NULL){
+        return amount;
+    }
+    else{
+        return countNode(currNode->next, amount++);
+    }
+}
+void linkList::showList(node *currNode){
+    if(currNode==NULL){
+        cout<<"End."<<endl;
+    }
+    else{
+        cout<<currNode->ch<<" ";
+        showList(currNode->next);
+    }
+}
+hashTable::hashTable(string txtIn){
+    size=txtIn.length();
+    arrayChar[size]=new linkList[size];
+}
+int main(){
+    hashTable *table1=new hashTable("kkk");
+
+}
