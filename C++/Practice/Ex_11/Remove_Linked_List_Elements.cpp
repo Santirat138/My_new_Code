fc65ba54eb2627c8c0b33594beb59a3add368c5d
Santirat138 <pleumsantirat@gmail.com>
2024-01-29 09:02:48 +0700

    Update Remove_Linked_List_Elements.cpp

--- C++/Practice/Ex_11/Remove_Linked_List_Elements.cpp
+++ C++/Practice/Ex_11/Remove_Linked_List_Elements.cpp
@@ -13,6 +13,8 @@ class linkList{
         void addFirst(int newNum);
         void showList(node *currNode);
         node *findNum(int numIn, node *currNode);
+        node *findPrev(node *currNode, node *targetNode);
+        void deleteNode(node *nodeIn);
         void removeNum(int numIn);
 };
 node::node(int numIn){
@@ -49,12 +51,52 @@ node *linkList::findNum(int numIn, node *currNode){
         return findNum(numIn, currNode->next);
     }
 }
+node *linkList::findPrev(node *currNode ,node *targetNode){
+    if(currNode==NULL){
+        return NULL;
+    }
+    else if(currNode->next==targetNode){
+        return currNode;
+    }
+    else{
+        return findPrev(currNode->next, targetNode);
+    }
+}
+void linkList::deleteNode(node *nodeIn){
+    if(nodeIn==head){
+        head=nodeIn->next;
+        nodeIn->next=NULL;
+    }
+    else if(nodeIn->next==NULL){
+        node *prevTail=findPrev(head, nodeIn);
+        prevTail->next=NULL;
+    }
+    else{
+        node *prevNode=findPrev(head, nodeIn);
+        prevNode->next=nodeIn->next;
+    }
+
+}
 void linkList::removeNum(int numIn){
-    node *delNode=findNum(numIn, head);
+    node *delNode=head;
     while(delNode!=NULL){
-        
+        if(delNode->num==numIn){
+            deleteNode(delNode);
+        }
+        delNode=delNode->next;
     }
 }
 int main(){
-
+    linkList *list1=new linkList();
+    list1->addFirst(0);
+    list1->addFirst(1);
+    list1->addFirst(1);
+    list1->addFirst(1);
+    list1->addFirst(2);
+    list1->addFirst(3);
+    list1->addFirst(1);
+    
+    list1->showList(list1->head);
+    list1->removeNum(1);
+    list1->showList(list1->head);
 }
