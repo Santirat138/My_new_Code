500711d9d8ec42593c5c878d9553a1c4ed29e765
Santirat138 <pleumsantirat@gmail.com>
2024-02-09 08:07:40 +0700

    Build new leetCode.

---
+++ C++/LeetCode/NotDone/L83_Remove_Duplicates_from_Sorted_List/Remove Duplicates_from_Sorted_List.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+class node{
+    public:
+        int num;
+        node *next;
+        node(int numIn);
+};
+class linkList{
+    public:
+        node *head;
+        node *tail;
+        linkList();
+        void addLast(int newNum);
+};
+node::node(int numIn){
+    num=numIn;
+    next=NULL;
+}
+linkList::linkList(){
+    head=NULL;
+    tail=NULL;
+}
+void linkList::addLast(int newNum){
+    
+}
+int main(){
+
+}
---
+++ Java/LeetCode/NotDone/L9_Palindrome_Number/L9_Palindrome_Number.java
@@ -0,0 +1,7 @@
+package LeetCode.NotDone.L9_Palindrome_Number;
+
+public class L9_Palindrome_Number {
+    public static void main(String[] args){
+        System.out.println("Hello");
+    }
+}
